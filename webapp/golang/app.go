bafda9cb244d9c82f31e3bbbbab90d7840ff2848
wind111-lang <shota.tsutsui@prtimes.co.jp>
2024-07-31 13:39:55 +0900

    dbinitialize

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -80,9 +80,9 @@ func dbInitialize() {
 	sqls := []string{
 		"DELETE FROM users WHERE id > 1000",
 		"DELETE FROM posts WHERE id > 10000",
-		"DELETE FROM comments WHERE id > 10000",
-		"UPDATE users SET del_flg = 0 WHERE id % 2 = 0",
-		"UPDATE users SET del_flg = 1 WHERE id % 10 = 0",
+		"DELETE FROM comments WHERE id > 100000",
+		"UPDATE users SET del_flg = 0",
+		"UPDATE users SET del_flg = 1 WHERE id % 50 = 0",
 	}
 
 	for _, sql := range sqls {
