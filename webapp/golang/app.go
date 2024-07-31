358f31033dca1bd3f1dde18142237cb854691e6e
wind111-lang <shota.tsutsui@prtimes.co.jp>
2024-07-31 14:18:01 +0900

    readfile

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"bufio"
 	crand "crypto/rand"
 	"fmt"
 	"html/template"
-	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -633,7 +633,7 @@ func postIndex(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	filedata, err := io.ReadAll(file)
+	filedata, err := bufio.NewReader(file).Peek(UploadLimit)
 	if err != nil {
 		log.Print(err)
 		return
